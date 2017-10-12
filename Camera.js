ab17aa6f3d2a3e761f6b37fcafffbab6f87f1544
Serhij Korochanskyj <webzepter@gmail.com>
2017-10-12 14:46:20 +0300

    Fix React.PropTypes

--- Camera.js
+++ Camera.js
@@ -1,4 +1,5 @@
-import React, {Component, PropTypes} from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types'
 import {NativeAppEventEmitter, NativeModules, Platform, StyleSheet, requireNativeComponent, View} from 'react-native';
 
 const CameraManager = NativeModules.CameraManager || NativeModules.CameraModule;
